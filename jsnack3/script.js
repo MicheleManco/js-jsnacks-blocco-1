cbf003902692d7daba45170bab147504ea39143c
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-28 15:10:18 +0200

    finito

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,28 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
+
+// chiedo dieci volte all'utente di mettere dei numeri
+    // creo un array per contenere tutti i numeri che mi inserirà l'utente
+    // inserisco dentro l'array tutti i dieci dati che mi genera l'utente
+
+let arrayUser = [];
+
+while (arrayUser.length < 10 ){
+   let numero =  parseInt(prompt("inserisci un numero"));
+   arrayUser.push(numero)
+   console.log(numero);
+}
+console.log(arrayUser);
+
+//fai la somma di tutti i numeri dentro l'array
+//creare una variabile che mi prende la somma di tutti i numeri  
+//creare un for che aggiunge alla variabile un altro i'esimo 
+//stampare la somma 
+
+let somma = 0;
+for (let i = 0; i < arrayUser.length; i++) {
+    somma = arrayUser[i] + somma;
+}
+
+console.log(somma)
+
