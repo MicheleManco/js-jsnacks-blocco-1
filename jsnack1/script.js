eea070a7a224bc7f8c63eb1108aacf1e23856b87
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-28 14:37:23 +0200

    finito

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,16 @@
 // L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
+
+// prendo i due numeri che mi da l'utente
+let firstNum = parseInt(prompt("inserisci un numero"));
+let secondNum = parseInt(prompt("inserisci un numero"));
+
+//confronto i due numeri e metto in console il maggiore
+if (firstNum < secondNum) {
+    console.log(secondNum)
+} else if (firstNum > secondNum) {
+    console.log(firstNum);
+}else {
+    console.log("hai scelto due numeri uguali")
+}
+
