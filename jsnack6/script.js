e627f7089478e8dc7b5e685a11e6586adb94806e
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-28 18:48:18 +0200

    finito tutto

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -1,22 +1,25 @@
 // Genera un array di 50 numeri random. Nell’array non devono esserci doppioni.
 
-// genero un array 
-// genero 50 numeri random senza doppioni
-    //faccio un for contenente 50 numeri generati random
-    //li pusho solo se non c'è ne gia uno dentro di uguale 
-        //per trovare se c'è ne uno uguale prendo tutti i numeri che entrano nel array e li confronto
-    //non devono esserci presenti dentro l'array numeri uguali
+//devo generare un array vuoto che dovro popolare con un ciclo
+//devo generare 50 numeri => un ciclo che mi ritorni 50 numeri casuali
+//questi 50 numeri devo confrontarli con tutti i numeri dento l'array quindi devo attraversare tutto l'array quindi for
+    //creo una variabile che mi permetta di mettere oppure no i numeri dentro l'array 
+    // se  array[i] == il numero generato, la variabile cambia e mi salta il push 
+    //di conseguenza se la variabile non cambia pusho 
+
 
 let arrayNum = []
 
-for(let i = 0; i < 10; i++){
-    let nums = Math.floor(Math.random()*10+1)
-    if(nums != arrayNum){
-        arrayNum.push(nums)
-    }else {
-        i--
+while (arrayNum.length < 10){
+    let doppio = false;
+    let numbers = Math.floor(Math.random()*10 +1);
+    for(let i = 0; i < arrayNum.length; i++){
+        if(arrayNum[i] == numbers){
+            doppio = true;
+        }
+    }
+    if (doppio == false){
+        arrayNum.push(numbers)
     }
 }
-console.log(arrayNum);
-
-//dopo che ho generato il numero random devo far attraversare a quel numero random tutto l'array che contiene gli altri numeri
+    console.log(arrayNum)
