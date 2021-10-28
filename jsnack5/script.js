3eb633bb01ee51592f58434aaf641caf7aff489a
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-28 15:38:01 +0200

    finito jsnack5

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,3 +1,19 @@
 // Crea un array vuoto.
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
+
+// creo un array vuoto
+//chiedo sei volte 
+    //guardo se sono dispari
+    //se sono dispari li aggiungo all'array
+    
+let numeriDis = [];
+
+for (let i = 0; i < 6 ; i++) {
+    let numUtente = parseInt(prompt("inserisci i tuoi sei numeri"));
+    if (numUtente % 2){
+        numeriDis.push(numUtente)
+    }
+}
+
+console.log(numeriDis);
